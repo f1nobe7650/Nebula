9b1af053499c3e5ae8e4d4aa70f9e17897c63277
finobe <107956925+f1nobe7650@users.noreply.github.com>
2024-11-10 13:21:00 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,2 @@
-local is_solara = string.find(identifyexecutor(), "Solara")
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f91b8a81c3e4e02ef7847d58f9c33e3f.lua"))()
 
-if is_solara then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f91b8a81c3e4e02ef7847d58f9c33e3f.lua"))()
-else 
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7b52da8a5e0ca4d8580cb4381d461f23.lua"))()
-end 
